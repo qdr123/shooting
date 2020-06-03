5426a23f1d3233da65c2be34b0cf7d58d26d9d1a
qdr123 <tmxjdydrb12@gmail.com>
2020-06-03 17:42:43 +0900

    플레이어 총알 발사 이펙트
    -총 사운드
    -드럼통 폭발음
    -플레이어 총발사 및 사운드

--- New Unity Project/Assets/2.Scripts/RemoveBullet.cs
+++ New Unity Project/Assets/2.Scripts/RemoveBullet.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RemoveBullet : MonoBehaviour
 {
+
+    //스파크 프리팹을 저장할 변수
+    public GameObject sparkEffect;
     //충돌이 시작할때 발생하는 이벤트
     private void OnCollisionEnter(Collision coll)
     {
         //충돌한 게임오브젝트의 태그값 비교
         if(coll.collider.tag=="BULLET")
         {
+            //스파크 효과 함수 호출
+            showEffect(coll);
             //충돌한 게임오브젝트 삭제
             Destroy(coll.gameObject);
+            
 
         }
+    }
+
+    private void showEffect(Collision coll)
+    {
+        //출동 지점의 정보를 추출
+        ContactPoint contact = coll.contacts[0];
+        //법선 벡터가 이루는 회전각도를 추출
+        Quaternion rot = Quaternion.FromToRotation(-Vector3.forward, contact.normal);
+
+        //스파크 효과를 생성
+        Instantiate(sparkEffect, contact.point, rot);
     }
 }
